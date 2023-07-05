aea6683a807f8072058a2b5a27f3edb9e73ede8c
Matheus Motta <matheusdomingues423@gmail.com>
2023-07-05 09:06:54 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
-import fastify from "fastify";
 import jwt from "@fastify/jwt";
+import cors from "@fastify/cors";
+import fastify from "fastify";
 
 import { authRoutes } from "./routes/auth";
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true
+})
+
 app.register(jwt, {
-  secret: 'wdj2nefhduxrfwjdk'
+  secret: 'studyio'
 })
 
 app.register(authRoutes)
 
 app
-  .listen({ port: 3333 })
+  .listen({ port: 3333, host: '0.0.0.0' })
   .then(() => console.log('server running...'))
